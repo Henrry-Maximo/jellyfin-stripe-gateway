cd9418650635a60c080662cfa45067b9f659736b
Henrry-Maximo <henrrylimadasilva@gmail.com>
2026-07-26 01:03:55 -0300

    ✨ feat: Add form sign in styles

--- web/src/pages/_layouts/auth.tsx
+++ web/src/pages/_layouts/auth.tsx
@@ -1,11 +1,10 @@
-import logo from '@/assets/capa.png';
 import cinemaVideo from '@/assets/cinema.mp4';
 import { Outlet } from 'react-router-dom';
 
 export function AuthLayout() {
   return (
     <div className="grid h-screen grid-cols-1 overflow-hidden antialiased">
-      <div className="relative hidden flex-col items-center justify-center overflow-hidden bg-zinc-950 lg:flex">
+      <div className="relative flex-col items-center justify-center overflow-hidden bg-zinc-950 lg:flex">
         <video
           src={cinemaVideo}
           autoPlay
@@ -15,11 +14,11 @@ export function AuthLayout() {
           className="absolute inset-0 h-full w-full object-cover opacity-100 blur-sm"
         />
         <div className="absolute inset-0 bg-zinc-950/60" />
-        <div className="relative z-10 flex h-full w-full flex-col gap-6 border-r-2 border-purple-600 px-4 py-2">
+        <div className="relative z-10 flex h-full w-full flex-col px-2">
           <header>
             <p className="text-sm text-zinc-600">Jelly Gateway Web.</p>
           </header>
-          <main className="z-10 flex flex-1 flex-col items-center justify-center gap-6">
+          <main className="z-10 flex flex-1 flex-col items-center justify-center">
             <Outlet />
           </main>
           <footer className="flex justify-between">
